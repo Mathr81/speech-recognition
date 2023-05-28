32b5ff29f6e8235572771c1970a6860535a820f4
Mathr81 <112965202+Mathr81@users.noreply.github.com>
2023-05-28 17:38:26 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+recognition.continuous = true;
+recognition.interimResults = true;
+recognition.lang = 'fr-FR';
+
+let transcription = '';
+
+recognition.onresult = (event) => {
+  const results = event.results;
+  const interimTranscription = '';
+
+  for (let i = event.resultIndex; i < results.length; i++) {
+    const transcript = results[i][0].transcript;
+    if (results[i].isFinal) {
+      transcription += transcript + ' ';
+    } else {
+      interimTranscription += transcript;
+    }
+  }
+
+  document.getElementById('transcription').innerHTML = transcription + interimTranscription;
+};
+
+recognition.start();
